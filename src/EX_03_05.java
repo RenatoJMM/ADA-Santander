943d3aa2e89cf32b32132eb21f2bcdc4955575a5
Renato Magalhaes <renato.jm@hotmail.com>
2023-07-17 22:26:30 -0300

    Adding last exercice of LMS list

--- src/EX_03_05.java
+++ src/EX_03_05.java
@@ -1,10 +1,24 @@
 public class EX_03_05 {
 
     public static boolean elementosRepetidos(double[] input) {
-        return true;
+
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] == input[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     public static void main(String[] args) {
+
+        // double[] input = { 2.5, 3.2, 4.5, 5, 6 };
+        double[] input = { 2, 2, 3.1, 4, 4.1, 4.1 };
+
+        System.out.println(elementosRepetidos(input));
 
     }
 
